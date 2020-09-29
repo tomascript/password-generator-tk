2a82798a0f379cb1afe58ec9bef998be0fe93203
tomascript <tomas.ko@centrum.sk>
2020-09-29 13:42:11 +0200

    javascript complete remake

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const $toolTip = document.querySelector('#tooltip');
 const $form = document.querySelector('.form');
 const $checkOptions = document.querySelectorAll('input[type="checkbox"]');
 const $resultField = document.querySelector('#result');
-const defaultMessage = 'Enter length between 5 - 30';
+const defaultMessage = `Enter length between ${$numberField.min} - ${$numberField.max}`;
 
 // 97-122 lowercase letters
 // 65-90 uppercase letters
@@ -12,29 +12,18 @@ const defaultMessage = 'Enter length between 5 - 30';
 // 58-64 special chars2
 // 91-96 special chars3
 // 123-126 special chars4
-const asciiRanges = {
-  lowerCase: {
-    ranges: [{ startsAt: 97, count: 26 }]
-  },
-  upperCase: {
-    ranges: [{ startsAt: 65, count: 26 }]
-  },
-  numbers: {
-    ranges: [{ startsAt: 48, count: 10 }]
-  },
-  special: {
-    ranges: [
-      { startsAt: 33, count: 15 },
-      { startsAt: 58, count: 7 },
-      { startsAt: 91, count: 6 },
-      { startsAt: 123, count: 4 }
-    ]
-  }
-};
+
+const LOWER_CASE_ARRAY = populateArray(97, 122);
+const UPPER_CASE_ARRAY = populateArray(65, 90);
+const NUMBERS_ARRAY = populateArray(48, 57);
+const SYMBOLS_ARRAY = populateArray(33, 47)
+  .concat(populateArray(58, 64))
+  .concat(populateArray(91, 96))
+  .concat(populateArray(123, 126));
 
 $numberField.addEventListener('input', e => {
   const val = e.target.value;
-  if (val < 5 || val > 30 || isNaN(val)) {
+  if (val < +$numberField.min || val > +$numberField.max || isNaN(val)) {
     $toolTip.textContent = defaultMessage;
     $toolTip.classList.add('open');
   } else {
@@ -51,146 +40,62 @@ $checkOptions.forEach(opt =>
 
 $form.addEventListener('submit', e => {
   e.preventDefault();
-  generatePassword();
-});
-
-function generatePassword() {
-  let text = '';
-  let charCount = $numberField.value;
-  if (charCount < 5 || charCount > 30 || isNaN(charCount)) return;
-  let isNumber = document.querySelector('#is-number').checked;
-  let isUpper = document.querySelector('#upper-case').checked;
-  let isSpecial = document.querySelector('#is-special').checked;
-  for (let i = 0; i < charCount; i++) {
-    let generatedChar = generateCharacter(isNumber, isUpper, isSpecial);
-    while (generatedChar === text.slice(-1)) {
-      generatedChar = generateCharacter(isNumber, isUpper, isSpecial);
-    }
-    text += generatedChar;
+  let asciiCodeArray = [...LOWER_CASE_ARRAY];
+  if ($checkOptions[0].checked) {
+    asciiCodeArray = [...asciiCodeArray, ...UPPER_CASE_ARRAY];
   }
-  $resultField.textContent = text;
+  if ($checkOptions[1].checked) {
+    asciiCodeArray = [...asciiCodeArray, ...NUMBERS_ARRAY];
+  }
+  if ($checkOptions[2].checked) {
+    asciiCodeArray = [...asciiCodeArray, ...SYMBOLS_ARRAY];
+  }
+  let password = generatePassword(asciiCodeArray, $numberField.value);
+  while (!checkPassword(password)) {
+    password = generatePassword(asciiCodeArray, $numberField.value);
+  }
+  $resultField.textContent = password;
   $resultField.select();
   document.execCommand('copy');
-  $toolTip.textContent = 'Copied to Clipboard!';
+  $toolTip.textContent = 'Copied to Clipboard';
   $toolTip.classList.add('open');
-}
+});
 
-function generateCharacter(isNumber, isUpper, isSpecial) {
-  let temp, idx;
-  if (isUpper && isNumber && isSpecial) {
-    temp = String.fromCharCode(Math.round(Math.random() * 93) + 33);
-  } else if (isUpper && isNumber) {
-    idx = Math.floor(Math.random() * 3);
-    switch (idx) {
-      case 0:
-        temp = String.fromCharCode(
-          Math.floor(Math.random() * asciiRanges.numbers.ranges[0].count) +
-            asciiRanges.numbers.ranges[0].startsAt
-        );
-        break;
-      case 1:
-        temp = String.fromCharCode(
-          Math.floor(Math.random() * asciiRanges.upperCase.ranges[0].count) +
-            asciiRanges.upperCase.ranges[0].startsAt
-        );
-        break;
-      case 2:
-        temp = String.fromCharCode(
-          Math.floor(Math.random() * asciiRanges.lowerCase.ranges[0].count) +
-            asciiRanges.lowerCase.ranges[0].startsAt
-        );
-        break;
-      default:
-        temp = null;
-    }
-  } else if (isNumber && isSpecial) {
-    idx = Math.floor(Math.random() * 3);
-    switch (idx) {
-      case 0:
-        temp = String.fromCharCode(
-          Math.floor(Math.random() * asciiRanges.numbers.ranges[0].count) +
-            asciiRanges.numbers.ranges[0].startsAt
-        );
-        break;
-      case 1:
-        idx = Math.floor(Math.random() * 4);
-        temp = String.fromCharCode(
-          Math.floor(Math.random() * asciiRanges.special.ranges[idx].count) +
-            asciiRanges.special.ranges[idx].startsAt
-        );
-        break;
-      case 2:
-        temp = String.fromCharCode(
-          Math.floor(Math.random() * asciiRanges.lowerCase.ranges[0].count) +
-            asciiRanges.lowerCase.ranges[0].startsAt
-        );
-        break;
-      default:
-        temp = null;
-    }
-  } else if (isUpper && isSpecial) {
-    idx = Math.floor(Math.random() * 3);
-    switch (idx) {
-      case 0:
-        temp = String.fromCharCode(
-          Math.floor(Math.random() * asciiRanges.upperCase.ranges[0].count) +
-            asciiRanges.upperCase.ranges[0].startsAt
-        );
-        break;
-      case 1:
-        idx = Math.floor(Math.random() * 4);
-        temp = String.fromCharCode(
-          Math.floor(Math.random() * asciiRanges.special.ranges[idx].count) +
-            asciiRanges.special.ranges[idx].startsAt
-        );
-        break;
-      case 2:
-        temp = String.fromCharCode(
-          Math.floor(Math.random() * asciiRanges.lowerCase.ranges[0].count) +
-            asciiRanges.lowerCase.ranges[0].startsAt
-        );
-        break;
-      default:
-        temp = null;
-    }
-  } else if (isNumber) {
-    Math.round(Math.random())
-      ? (temp = String.fromCharCode(
-          Math.floor(Math.random() * asciiRanges.numbers.ranges[0].count) +
-            asciiRanges.numbers.ranges[0].startsAt
-        ))
-      : (temp = String.fromCharCode(
-          Math.floor(Math.random() * asciiRanges.lowerCase.ranges[0].count) +
-            asciiRanges.lowerCase.ranges[0].startsAt
-        ));
-  } else if (isUpper) {
-    Math.round(Math.random())
-      ? (temp = String.fromCharCode(
-          Math.floor(Math.random() * asciiRanges.upperCase.ranges[0].count) +
-            asciiRanges.upperCase.ranges[0].startsAt
-        ))
-      : (temp = String.fromCharCode(
-          Math.floor(Math.random() * asciiRanges.lowerCase.ranges[0].count) +
-            asciiRanges.lowerCase.ranges[0].startsAt
-        ));
-  } else if (isSpecial) {
-    idx = Math.floor(Math.random() * 4);
-    Math.round(Math.random())
-      ? (temp = String.fromCharCode(
-          Math.floor(Math.random() * asciiRanges.special.ranges[idx].count) +
-            asciiRanges.special.ranges[idx].startsAt
-        ))
-      : (temp = String.fromCharCode(
-          Math.floor(Math.random() * asciiRanges.lowerCase.ranges[0].count) +
-            asciiRanges.lowerCase.ranges[0].startsAt
-        ));
-  } else {
-    temp = String.fromCharCode(
-      Math.floor(Math.random() * asciiRanges.lowerCase.ranges[0].count) +
-        asciiRanges.lowerCase.ranges[0].startsAt
-    );
+function populateArray(startIndex, endIndex) {
+  const arr = [];
+  for (let i = startIndex; i <= endIndex; i++) {
+    arr.push(i);
   }
-  return temp;
+  return arr;
 }
 
-// console.log(text);
+function generatePassword(arr, charCount) {
+  let nbr;
+  let passwordCodes = [];
+  for (let i = 0; i < charCount; i++) {
+    nbr = arr[Math.floor(Math.random() * arr.length)];
+    while (nbr == passwordCodes[passwordCodes.length - 1]) {
+      nbr = arr[Math.floor(Math.random() * arr.length)];
+    }
+    passwordCodes.push(nbr);
+  }
+  return String.fromCharCode(...passwordCodes);
+}
+
+function checkPassword(pass) {
+  let valid = true;
+  if ($checkOptions[0].checked) {
+    valid = valid && /[A-Z]/.test(pass);
+    if (!valid) return valid;
+  }
+  if ($checkOptions[1].checked) {
+    valid = valid && /\d/.test(pass);
+    if (!valid) return valid;
+  }
+  if ($checkOptions[2].checked) {
+    valid = valid && !/^[a-zA-Z0-9- ]*$/.test(pass);
+    if (!valid) return valid;
+  }
+  valid = valid && /[a-z]/.test(pass);
+  return valid;
+}
